6dab4a1a554cf7eb7696a5e130b96e1ba25c0d6d
Haliam <haliampf@gmail.com>
2023-02-14 13:58:50 +0000

    Add first problem CountZeros

---
+++ Algorithms/Easy/CountZeros/CountZerosV2.cs
@@ -0,0 +1,58 @@
+﻿namespace Algorithms.Easy.CountZeros;
+
+public class CountZerosV2
+{
+    public static int CalculateMaxZerosBetweenTwoOne(string number)
+    {
+        int numberOfZeros = 0;
+        List<int> indexOfOneList = new List<int>();
+        List<int> numberOfZerosList = new List<int>();
+
+        if (!IsValid(number))
+            return 0;
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            if (number[i] == '1')
+            {
+                indexOfOneList.Add(i);
+            }
+        }
+
+        for (int i = indexOfOneList.Count - 1; i > 0; i--)
+        {
+            var j = i - 1;
+            numberOfZeros = indexOfOneList[i] - indexOfOneList[j] - 1;
+            numberOfZerosList.Add(numberOfZeros);
+        }
+
+        return numberOfZerosList.Max();
+    }
+
+    private static bool IsValid(string number)
+    {
+        int countOne = 0;
+        int countZero = 0;
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            if (number[i] == '1')
+                countOne++;
+
+            if (number[i] == '0')
+                countZero++;
+
+            if (number[i] != '0' && number[i] != '1')
+            {
+                return false;
+            }
+        }
+
+        if (countOne == 0 ||
+            countOne == 1 ||
+            countZero == 0)
+            return false;
+
+        return true;
+    }
+}
---
+++ Algorithms/Easy/CountZeros/CountZerosV4.cs
@@ -0,0 +1,65 @@
+﻿namespace Algorithms.Easy.CountZeros;
+
+public class CountZerosV4
+{
+    public static int CalculateMaxZerosBetweenTwoOne(string number)
+    {
+
+        if (!IsValid(number))
+            return 0;
+
+        int maxZeros = 0;
+        int countZeros = 0;
+        int countOnes = 0;
+        int n = 0;
+        int[] array = new int[2];
+
+        //010100011
+        for (int i = n; i < number.Length; i++)
+        {
+
+            if (number[i] == '1')
+                countOnes++;
+
+            if (countOnes == 1 && number[i] != '0')
+                array[0] = i;
+
+            if (countOnes == 2)
+            {
+                array[1] = i;
+                n = i - 1;
+                countOnes = 0;
+                countZeros = (array[1] - array[0]) - 1;
+                maxZeros = countZeros;
+            }
+
+            if (countZeros > maxZeros)
+            {
+                maxZeros = countZeros;
+            }
+
+        }
+
+        return maxZeros;
+    }
+
+    private static bool IsValid(string number)
+    {
+        int countOnes = 0;
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            if (number[i] != '0' && number[i] != '1')
+                return false;
+
+            if (number[i] == '1')
+                countOnes++;
+        }
+
+        if (countOnes == 0 ||
+            countOnes == 1)
+            return false;
+
+        return true;
+    }
+}
