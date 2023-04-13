77eea50755f031dd062745f1c4c046c17d395f0a
Haliam <haliampf@gmail.com>
2023-04-13 22:31:55 +0100

    Add basic definition class for fish problem

---
+++ Algorithms/Easy/Fish/FishSolution.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.Easy.Fish
+{
+    public class FishSolution
+    {
+        public static int GetNumberOfLiveFish(int[] a, int[] b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
