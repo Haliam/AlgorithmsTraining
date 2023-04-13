f77096aa2d3964857427dbcdf2c1502d34e90242
Haliam <haliampf@gmail.com>
2023-04-13 22:31:22 +0100

    Add Fish problem test

---
+++ Algorithms.Test/Fish/FishData.cs
@@ -0,0 +1,16 @@
+﻿namespace Algorithms.Test.Fish
+{
+    public class FishData
+    {
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                new int[]{ 4, 3, 2, 1, 5 },
+                new int[]{ 0, 1, 0, 0, 0 },
+                2
+            },
+        };
+    }
+}
