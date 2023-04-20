a424679b63bb548d5f7b4cdf7b923247b75e1d5e
Haliam <haliampf@gmail.com>
2023-04-20 19:27:06 +0100

    Add direction and fish classes

---
+++ Algorithms/Easy/Fish/Direction.cs
@@ -0,0 +1,8 @@
+﻿namespace Algorithms.Easy.Fish
+{
+    public enum Direction
+    {
+        Up = 1,
+        Down = 2
+    }
+}
---
+++ Algorithms/Easy/Fish/Fish.cs
@@ -0,0 +1,11 @@
+﻿namespace Algorithms.Easy.Fish
+{
+    public class Fish
+    {
+        public int Id { get; set; }
+
+        public Direction Direction { get; set; }
+
+        public int Size { get; set; }
+    }
+}
