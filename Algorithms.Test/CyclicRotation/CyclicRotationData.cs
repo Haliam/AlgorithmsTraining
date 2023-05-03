0a7b2606ada3f01414fd36bd7deacbacb7c18ac2
Haliam <haliampf@gmail.com>
2023-05-03 15:53:18 +0100

    Add some data for testing failure cases

--- Algorithms.Test/CyclicRotation/CyclicRotationData.cs
+++ Algorithms.Test/CyclicRotation/CyclicRotationData.cs
@@ -34,6 +34,30 @@
                 1,
                 new int[]{ 1, 0, 0, 1 },
                 new int[]{ 1, 1, 0, 0 }
+            },
+            new object[]
+            {
+                0,
+                new int[]{ 1, 0, 0, 1 },
+                new int[]{ 1, 0, 0, 1 }
+            },
+            new object[]
+            {
+                1,
+                new int[]{ 1000, 8, 9 },
+                new int[]{ 9, 1000, 8}
+            },
+            new object[]
+            {
+                2,
+                new int[]{ -1000, 8, 9 },
+                new int[]{ 8, 9, -1000 }
+            },
+            new object[]
+            {
+                1,
+                Enumerable.Range(0, 100).ToArray(),
+                new int[]{99, 0}.Concat(Enumerable.Range(1,98)).ToArray(),
             }
           };
 
@@ -42,8 +66,34 @@
           {
             new object[]
             {
-
+                -1,
+                new int[]{ 3, 8, 9 },
+                new int[]{}
             },
+            new object[]
+            {
+                101,
+                new int[]{ 3, 8, 9 },
+                new int[]{}
+            },
+            new object[]
+            {
+                1,
+                new int[]{ -1001, 8, 9 },
+                new int[]{}
+            },
+            new object[]
+            {
+                1,
+                new int[]{ 1001, 8, 9 },
+                new int[]{}
+            },
+            new object[]
+            {
+                1,
+                Enumerable.Range(0, 101).ToArray(),
+                new int[]{}
+            }
           };
     }
 }
