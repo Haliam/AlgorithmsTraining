d885e6ee451920089e7b62a92ff853927ece178f
Haliam <haliampf@gmail.com>
2023-05-03 02:48:45 +0100

    Define validation structure and implement GetRotatedArray

---
+++ Algorithms/Easy/CyclicRotation/CyclicRotationArray.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms.Easy.CyclicRotation
+{
+    public static class CyclicRotationArray
+    {
+        public static int[] GetRotatedArray(int k, int[] array)
+        {
+            if (!CyclicRotationValidation.IsValid(k, array))
+                return Array.Empty<int>();
+
+            for (int i = 0; i < k; i++)
+                array = Rotating(array);
+
+            return array;
+        }
+
+        private static int[] Rotating(int[] array)
+        {
+            int[] firstArray = new int[2];
+
+            int[] secondArray = new int[array.Length - 2];
+
+            firstArray[0] = array[array.Length -1];
+
+            firstArray[1] = array[0];
+
+            for (int i = 0; i < secondArray.Length; i++)
+                secondArray[i] = array[i + 1];
+
+            return firstArray.Concat(secondArray).ToArray();
+        }
+    }
+}
---
+++ Algorithms/Easy/CyclicRotation/CyclicRotationValidation.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithms.Easy.CyclicRotation
+{
+    public static class CyclicRotationValidation
+    {
+        public static bool IsValid(int k, int[] array)
+        {
+
+
+            return true;
+        }
+    }
+}
