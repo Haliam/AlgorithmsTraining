166f836e33f7bb12ad60251f680892671bc41c63
Haliam <haliampf@gmail.com>
2023-05-03 02:41:16 +0100

    Add some data and change method name

--- Algorithms.Test/CyclicRotation/CyclicRotationData.cs
+++ Algorithms.Test/CyclicRotation/CyclicRotationData.cs
@@ -1,13 +1,19 @@
 ﻿namespace Algorithms.Test.CyclicRotation
 {
-    public class CyclicRotationData
+    public static class CyclicRotationData
     {
         public static IEnumerable<object[]> CyclicRotationSuccess =>
           new List<object[]>
           {
             new object[]
             {
-                3, 
+                2,
+                new int[]{ 3, 8, 9 },
+                new int[]{ 8, 9, 3 }
+            },
+            new object[]
+            {
+                3,
                 new int[]{ 3, 8, 9, 7, 6 },
                 new int[]{ 9, 7, 6, 3, 8 }
             },
@@ -17,6 +23,18 @@
                 new int[]{ 1, 2, 3, 4 },
                 new int[]{ 1, 2, 3, 4 }
             },
+            new object[]
+            {
+                1,
+                new int[]{ 0, 0, 0, 0 },
+                new int[]{ 0, 0, 0, 0 }
+            },
+            new object[]
+            {
+                1,
+                new int[]{ 1, 0, 0, 1 },
+                new int[]{ 1, 1, 0, 0 }
+            }
           };
 
         public static IEnumerable<object[]> CyclicRotationFail =>
--- Algorithms.Test/CyclicRotation/CyclicRotationTest.cs
+++ Algorithms.Test/CyclicRotation/CyclicRotationTest.cs
@@ -6,7 +6,7 @@
         [MemberData(nameof(CyclicRotationData.CyclicRotationSuccess), MemberType = typeof(CyclicRotationData))]
         public void CyclicRotationSuccess(int k, int[] array, int[] expected)
         {
-            var result = CyclicRotationArray.Rotate(k, array);
+            var result = CyclicRotationArray.GetRotatedArray(k, array);
 
             result.Should().BeEquivalentTo(expected);
         }
@@ -15,7 +15,7 @@
         [MemberData(nameof(CyclicRotationData.CyclicRotationFail), MemberType = typeof(CyclicRotationData))]
         public void CyclicRotationFail(int k, int[] array, int[] expected)
         {
-            var result = CyclicRotationArray.Rotate(k, array);
+            var result = CyclicRotationArray.GetRotatedArray(k, array);
 
             result.Should().BeEquivalentTo(expected);
         }
