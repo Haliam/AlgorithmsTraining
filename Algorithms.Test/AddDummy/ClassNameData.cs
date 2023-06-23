a4ad008e7727e4fe075d2c5fabdf5ddfb944feb3
Haliam <haliampf@gmail.com>
2023-06-23 15:08:54 +0100

    Add Dummy

---
+++ Algorithms.Test/AddDummy/ClassNameData.cs
@@ -0,0 +1,23 @@
+﻿namespace Algorithms.Test.AddDummy
+{
+    public static class ClassNameData
+    {
+        public static IEnumerable<object[]> ClassNameSuccess =>
+        new List<object[]>
+        {
+            new object[]
+            {
+
+            }
+        };
+
+        public static IEnumerable<object[]> ClassNameFail =>
+        new List<object[]>
+        {
+            new object[]
+            {
+
+            }
+        };
+    }
+}
---
+++ Algorithms.Test/AddDummy/ClassNameTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Algorithms.Test.AddDummy
+{
+    public class ClassNameTest
+    {
+        [Theory]
+        [MemberData(nameof(ClassNameData.ClassNameSuccess), MemberType = typeof(ClassNameData))]
+        public void ClassNameSuccess()
+        {
+
+        }
+
+        [Theory]
+        [MemberData(nameof(ClassNameData.ClassNameFail), MemberType = typeof(ClassNameData))]
+        public void ClassNameFail()
+        {
+
+        }
+    }
+}
