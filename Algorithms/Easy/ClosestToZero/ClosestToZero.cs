82eafa4fccdec5cbb49a17c31a0a3b73998f7ee3
Haliam <haliampf@gmail.com>
2023-06-23 15:11:26 +0100

    Add ClosestToZero

---
+++ Algorithms/Easy/ClosestToZero/ClosestToZero.cs
@@ -0,0 +1,41 @@
+﻿namespace Algorithms.Easy.ClosestToZero
+{
+    public class ClosestToZero
+    {
+        public static int ComputeClosestToZero(int[] ts)
+        {
+            if (!ClosestToZeroValidation.IsValid(ts))
+                return 0;
+
+            int temperature = GetClosestToZeroTemperature(ts);
+
+            return temperature;
+        }
+
+        private static int GetClosestToZeroTemperature(int[] ts)
+        {
+            int closestNegativeToZero = ts[0];
+            int closestPositiveToZero = ts[0];
+            int closestToZeroTemperature = 0;
+
+            foreach (int t in ts)
+            {
+                if (t < 0)
+                {
+                    closestNegativeToZero = t < closestNegativeToZero ? t : closestNegativeToZero;
+                }
+                else
+                {
+                    closestPositiveToZero = t < closestPositiveToZero ? t : closestPositiveToZero;
+                }
+            }
+
+            if (closestPositiveToZero == Math.Abs(closestNegativeToZero))
+                return closestPositiveToZero;
+
+            closestToZeroTemperature = closestPositiveToZero < Math.Abs(closestNegativeToZero) ? closestPositiveToZero : closestNegativeToZero;
+
+            return closestToZeroTemperature;
+        }       
+    }
+}
